e33444cfc736a904d65f817964f9f45d5386fd09
tc360950 <tc360950@students.mimuw.edu.pl>
2020-11-19 21:38:58 +0100

    excluded dummy coefficient from penalty

--- CTBN/CTBN/solvers/admm_solver.h
+++ CTBN/CTBN/solvers/admm_solver.h
@@ -15,7 +15,7 @@ private:
 	const size_t L2_ITERATIONS = 10;
 	const Real_t L2_STEP_SIZE = 0.1;
 	const Real_t EPSILON = 0.000000001;
-    const Real_t STOPPING_EPSILON = 0.0001;
+	const Real_t STOPPING_EPSILON = 0.0001;
 
 	void update_x(const std::vector<Real_t> &u, const std::vector<Real_t> &z, std::vector<Real_t> &x, const size_t node, const bool past_node_value, std::vector<Real_t> &gradient_holder) {
 		bool stop = false;
@@ -34,7 +34,8 @@ private:
 
 	void update_z(const std::vector<Real_t> &u, const std::vector<Real_t> &x, std::vector<Real_t> &z, const Real_t lambda) {
 		const Real_t k = lambda / RO;
-		for (size_t i = 0; i < z.size(); i++) {
+		z[0] = x[0] + u[0];
+		for (size_t i = 1; i < z.size(); i++) {
 			if (x[i] + u[i] > k) {
 				z[i] = x[i] + u[i] - k;
 			}
@@ -89,20 +90,20 @@ private:
 
 	Real_t get_vector_penalty(const std::vector<Real_t> &vec) {
 		Real_t result = 0.0;
-		for (size_t i = 0; i < vec.size(); i++) {
+		for (size_t i = 1; i < vec.size(); i++) {
 			result += std::abs(vec[i]);
 		}
 		return result;
 	}
-    
-    Real_t get_vector_l2_norm(const std::vector<Real_t> &vec) const {
-        Real_t result = 0.0;
+
+	Real_t get_vector_l2_norm(const std::vector<Real_t> &vec) const {
+		Real_t result = 0.0;
 		for (size_t i = 0; i < vec.size(); i++) {
 			result += vec[i] * vec[i];
 		}
 		return std::pow(result, 0.5);
-    }
-	
+	}
+
 	Real_t difference_norm(const std::vector<Real_t> &vec1, const std::vector<Real_t> &vec2) const {
 		Real_t result = 0.0;
 		for (size_t i = 0; i < vec1.size(); i++) {
@@ -111,12 +112,12 @@ private:
 		return std::pow(result, 0.5);
 	}
 
-    bool check_stop(const std::vector<Real_t> &u, const std::vector<Real_t> &x, const std::vector<Real_t> &z, const std::vector<Real_t> &old_u, 
-			const std::vector<Real_t> &old_z) const {
-        Real_t e_primal = std::sqrt(x.size()) * STOPPING_EPSILON + STOPPING_EPSILON * std::max(get_vector_l2_norm(x), get_vector_l2_norm(z));
-        Real_t e_dual = std::sqrt(x.size()) * STOPPING_EPSILON;
+	bool check_stop(const std::vector<Real_t> &u, const std::vector<Real_t> &x, const std::vector<Real_t> &z, const std::vector<Real_t> &old_u,
+		const std::vector<Real_t> &old_z) const {
+		Real_t e_primal = std::sqrt(x.size()) * STOPPING_EPSILON + STOPPING_EPSILON * std::max(get_vector_l2_norm(x), get_vector_l2_norm(z));
+		Real_t e_dual = std::sqrt(x.size()) * STOPPING_EPSILON;
 		return e_primal >= difference_norm(u, old_u) && e_dual >= RO * difference_norm(z, old_z);
-    }
+	}
 public:
 	ADMMSolver<Real_t>(LikelihoodCalculator<Real_t> likelihood_calculator) :
 		likelihood_calculator{ likelihood_calculator } {}
@@ -127,11 +128,11 @@ public:
 		std::vector<Real_t> u = get_starting_u(likelihood_calculator.get_parameters_size());
 		std::vector<Real_t> gradient_holder;
 		gradient_holder.resize(x.size());
-        if (DEBUG) {
-            log("Starting ADMM");
-            log("Score: ", get_vector_penalty(z) * lambda + likelihood_calculator.calculate_likelihood(z, node, past_node_value));
-            log("Penalty: ", get_vector_penalty(z) * lambda, " Likelihood: ", likelihood_calculator.calculate_likelihood(z, node, past_node_value));
-        }
+		if (DEBUG) {
+			log("Starting ADMM");
+			log("Score: ", get_vector_penalty(z) * lambda + likelihood_calculator.calculate_likelihood(z, node, past_node_value));
+			log("Penalty: ", get_vector_penalty(z) * lambda, " Likelihood: ", likelihood_calculator.calculate_likelihood(z, node, past_node_value));
+		}
 		bool stop = false;
 		size_t counter = 0;
 		while (!stop) {
@@ -149,13 +150,13 @@ public:
 			stop = check_stop(u, x, z, previous_u, previous_z);
 			counter++;
 		}
-        if (DEBUG) {
+		if (DEBUG) {
 			log("Stopped ADMM after ", counter, " iterations");
-            log("Finished optimization for node ", node, " past node value: ", (size_t) past_node_value, " with lambda ", lambda);
-            log("Score: ", get_vector_penalty(z) * lambda + likelihood_calculator.calculate_likelihood(z, node, past_node_value));
-            log("Penalty: ", get_vector_penalty(z) * lambda, " Likelihood: ", likelihood_calculator.calculate_likelihood(z, node, past_node_value));
-            log("Non zero vector elements: ", get_non_zero_vector_elements(z));
-        }
+			log("Finished optimization for node ", node, " past node value: ", (size_t)past_node_value, " with lambda ", lambda);
+			log("Score: ", get_vector_penalty(z) * lambda + likelihood_calculator.calculate_likelihood(z, node, past_node_value));
+			log("Penalty: ", get_vector_penalty(z) * lambda, " Likelihood: ", likelihood_calculator.calculate_likelihood(z, node, past_node_value));
+			log("Non zero vector elements: ", get_non_zero_vector_elements(z));
+		}
 		return std::make_tuple(z, likelihood_calculator.calculate_likelihood(z, node, past_node_value), get_non_zero_vector_elements(z));
 	}
 };
