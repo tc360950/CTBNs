6733ea646f439795a1118d54724b7a0b0a8fb7d1
tc360950 <37670208+tc360950@users.noreply.github.com>
2020-12-22 13:23:48 +0100

    Update bob_dylan.h

--- CTBN/CTBN/bob_dylan.h
+++ CTBN/CTBN/bob_dylan.h
@@ -11,7 +11,7 @@
 
 template <class Real_t, class Model> class BobDylan {
 private:
-	const size_t NUM_THREADS = 1;
+	const size_t NUM_THREADS = 50;
 	const size_t SOLVER_ITERATIONS = 10;
 	Real_t time_max_local;
 
